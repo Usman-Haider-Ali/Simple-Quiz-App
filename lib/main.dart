5fc6e379bd24dd8313aecb6eb822440f02bfb001
UsmanHaiderAli <theUsmanHaider@gmail.com>
2022-03-13 19:23:49 +0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:first_practice_app/widgets/button_widget.dart';
-import 'package:first_practice_app/widgets/question_widget.dart';
-import 'package:first_practice_app/widgets/result_widget.dart';
+import 'package:quiz_app/widgets/button_widget.dart';
+import 'package:quiz_app/widgets/question_widget.dart';
+import 'package:quiz_app/widgets/result_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
